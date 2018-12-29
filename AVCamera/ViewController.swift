278f821a6267c280f68116dde6318dd82509a345
Hồng Chinh <hongchinh@admins-imac-4.local>
2018-12-29 08:45:37 +0700

    Initial Commit

---
+++ AVCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVCamera
+//
+//  Created by Hồng Chinh on 12/29/18.
+//  Copyright © 2018 Hồng Chinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
